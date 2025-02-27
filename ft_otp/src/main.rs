ffd8ba2859ddfd548730e5182b28f91a0de14738
aurlic <arthur.urlic@gmail.com>
2025-02-27 15:42:14 +0100

    feat: use handle_g function

--- ft_otp/src/main.rs
+++ ft_otp/src/main.rs
@@ -1,3 +1,17 @@
+use arg::Args;
+use clap::Parser;
+
+mod arg;
+mod error;
+mod g_flag;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if let Some(key) = args.generate {
+        match g_flag::handle_g(key) {
+            Ok(_) => println!("File read successfully!"),
+            Err(e) => eprintln!("Error: {}", e),
+        }
+    }
 }
