95eba792a0771f11d976c1f84337cd7d31625156
aurlic <arthur.urlic@gmail.com>
2025-02-27 15:41:26 +0100

    feat: new error type for encryption error

--- ft_otp/src/error.rs
+++ ft_otp/src/error.rs
@@ -6,6 +6,7 @@ pub enum OTPError {
     FileNotFound(String),
     FileEmpty(String),
     InvalidKeyFormat,
+    EncryptionError,
 }
 
 impl fmt::Display for OTPError {
@@ -14,6 +15,7 @@ impl fmt::Display for OTPError {
             OTPError::FileNotFound(path) => write!(f, "File '{}' not found.", path),
             OTPError::FileEmpty(path) => write!(f, "File '{}' is empty.", path),
             OTPError::InvalidKeyFormat => write!(f, "Key must be 64 hexadecimal characters."),
+            OTPError::EncryptionError => write!(f, "Encryption failed"),
         }
     }
 }
