801ce345fd1783e1e092e3f8338ae863ab738427
aurlic <arthur.urlic@gmail.com>
2025-02-27 13:37:18 +0100

    feat: new custom error type

---
+++ ft_otp/src/error.rs
@@ -0,0 +1,21 @@
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+pub enum OTPError {
+    FileNotFound(String),
+    FileEmpty(String),
+    InvalidKeyFormat,
+}
+
+impl fmt::Display for OTPError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            OTPError::FileNotFound(path) => write!(f, "File '{}' not found.", path),
+            OTPError::FileEmpty(path) => write!(f, "File '{}' is empty.", path),
+            OTPError::InvalidKeyFormat => write!(f, "Key must be 64 hexadecimal characters."),
+        }
+    }
+}
+
+impl Error for OTPError {}
