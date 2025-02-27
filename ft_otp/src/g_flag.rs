c8de7bb56b8fcc8f35d6fb0d12fc0d75b73ff554
aurlic <arthur.urlic@gmail.com>
2025-02-27 15:43:04 +0100

    feat: parse key file + create random aes encryption key

---
+++ ft_otp/src/g_flag.rs
@@ -0,0 +1,45 @@
+use crate::error::OTPError;
+// use aes::cipher::{BlockEncrypt, NewCipher};
+// use aes::Aes256;
+// use hex::{decode, encode};
+use rand::RngCore;
+use std::fs::{self, File};
+use std::io::Write;
+use std::path::Path;
+
+fn is_hex(s: &str) -> bool {
+    s.chars().all(|c| c.is_ascii_hexdigit())
+}
+
+fn generate_random_key() -> Result<Vec<u8>, OTPError> {
+    let mut rng = rand::rng();
+    let mut key = vec![0u8; 32];
+    rng.fill_bytes(&mut key);
+
+    let mut file = File::create("aes.key").map_err(|_| OTPError::EncryptionError)?;
+    file.write_all(&key)
+        .map_err(|_| OTPError::EncryptionError)?;
+
+    Ok(key)
+}
+
+pub fn handle_g(key: String) -> Result<(), OTPError> {
+    let path = Path::new(&key);
+    if !path.exists() {
+        return Err(OTPError::FileNotFound(key));
+    }
+
+    let content = fs::read_to_string(&key).map_err(|_| OTPError::FileNotFound(key.clone()))?;
+    let trimmed_content = content.trim();
+    if trimmed_content.is_empty() {
+        return Err(OTPError::FileEmpty(key));
+    }
+    if trimmed_content.len() < 64 || !is_hex(trimmed_content) {
+        return Err(OTPError::InvalidKeyFormat);
+    }
+
+    let encryption_key = generate_random_key();
+
+    println!("✅ Key file '{}' is valid and saved securely!", key);
+    Ok(())
+}
